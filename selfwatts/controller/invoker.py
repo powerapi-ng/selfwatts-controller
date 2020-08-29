3c6679f1cac36e4bf66acd975f37648ed9e12f15
Guillaume Fieni <gfieni@users.noreply.github.com>
2020-08-29 15:20:19 +0200

    Add Invoker module

---
+++ selfwatts/controller/invoker.py
@@ -0,0 +1,43 @@
+from typing import List, Dict
+from subprocess import Popen
+
+
+class HwpcSensorInvoker:
+    """
+    HwPC-sensor invoker class.
+    """
+
+    def __init__(self, hostname: str, uri: str, database: str, collection: str):
+        self.hostname = hostname
+        self.uri = uri
+        self.database = database
+        self.collection = collection
+        self.process = None
+
+    def _generate_cmdline(self, events: List[str]) -> List[str]:
+        """
+        Generate the command line arguments to start the sensor.
+        """
+        cmdline = ['/usr/bin/hwpc-sensor']
+        cmdline += ['-n', self.hostname]
+        cmdline += ['-r', 'mongodb', '-U', self.uri, '-D', self.database, '-C', self.collection]
+        cmdline += ['-s', 'rapl', '-o', '-e', 'RAPL_ENERGY_PKG']
+        cmdline += ['-s', 'msr', '-e', 'TSC', '-e', 'APERF', '-e', 'MPERF']
+        cmdline += ['-c', 'core'] + [arg for event in events for arg in ('-e', event)]
+        return cmdline
+
+    def start(self, events: List[str]) -> None:
+        """
+        Start the sensor process.
+        """
+        if len(events) > 0:
+            self.process = Popen(self._generate_cmdline(events))
+
+    def stop(self) -> None:
+        """
+        Stop the sensor process.
+        """
+        if self.process is not None:
+            self.process.terminate()
+            self.process = None
+
